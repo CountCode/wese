cad7f285dd66e7d4107fb45cb185b229f5e6c3dd
Ilkka Lehikoinen <ilkka.lehikoinen@helsinki.fi>
2015-10-29 16:01:39 +0200

    week1 yleiskone

--- wk1-08_Yleiskone/src/code.js
+++ wk1-08_Yleiskone/src/code.js
@@ -9,11 +9,11 @@ function init() {
 function displaySection(index) {
     var sections = document.getElementsByTagName("section");
 
-    for(var i = 0; i < sections.length; i++) {
+    for (var i = 0; i < sections.length; i++) {
         if (index == i) {
-            sections[i].className='';
+            sections[i].className = '';
         } else {
-            sections[i].className='hidden';
+            sections[i].className = 'hidden';
         }
     }
 }
@@ -40,8 +40,43 @@ function miinus() {
 
 function jako() {
     var jakaja = haeNumero("toka");
-    if (jakaja===0) {
+    if (jakaja === 0) {
         alert("Nollalla ei voi jakaa");
     }
     asetaTulos(haeNumero("eka") / jakaja);
+}
+
+function celsiusFahrenheit() {
+    var celsius = haeNumero("celsius");
+    var fahrenheit = celsius * 9 / 5 + 32;
+    document.getElementById("fahrenheit").value = Math.round(fahrenheit);
+}
+
+function fahrenheitCelsius() {
+    var fahrenheit = haeNumero("fahrenheit");
+    var celsius = (fahrenheit - 32) * 5 / 9;
+    document.getElementById("celsius").value = Math.round(celsius);
+}
+
+function jalkaMetri() {
+    var jalka = haeNumero("jalka");
+    var metri = jalka * 0.3048;
+    document.getElementById("metri").value = Math.round(metri);
+}
+
+function metriJalka() {
+    var metri = haeNumero("metri");
+    var jalka = metri * 3.2808;
+    document.getElementById("jalka").value = Math.round(jalka);
+}
+
+function laske() {
+    var lohko0 = haeNumero("luokka0");
+    var lohko1 = haeNumero("luokka1");
+    var lohko2 = haeNumero("luokka2");
+    var lohko3 = haeNumero("luokka3");
+    console.log(lohko0);
+    var summa = lohko0 + lohko1 + lohko2 + lohko3;
+    console.log(summa);
+    document.getElementById("tulos").value = Math.round(summa);    
 }

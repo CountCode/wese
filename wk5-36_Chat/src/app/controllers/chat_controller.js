9782b7d053a1d36a9a5626c46f3dc10efa9ea26a
Ilkka Lehikoinen <ilkka.lehikoinen@helsinki.fi>
2015-11-30 14:53:57 +0200

    week5 chat done

--- wk5-36_Chat/src/app/controllers/chat_controller.js
+++ wk5-36_Chat/src/app/controllers/chat_controller.js
@@ -1,31 +1,37 @@
 ChatApp.controller('ChatController', function($scope, FirebaseService){
 	// Toteuta kontrolleri tänne
-//        $scope.messages = FireballService.getMessages();
-//        $scope.users = FireballService.getusers();
+        $scope.messages = FirebaseService.getMessages();
+        $scope.users = FirebaseService.getUsers();
         $scope.newText = '';
         $scope.newUser = '';
-//        $scope.activeUser;
-/*        
+        $scope.activeUser;
+        
         $scope.addMessage = function(){
+        //    console.log("add Message");
             if($scope.newText != ''){
-                var d = new Date();
+                var date = new Date();  // format?
+           //     console.log("Date "+date);
                 FirebaseService.addMessage({
                     text: $scope.newText,
                     user: $scope.activeUser,
-                    added: d  
+                    added:  date.toLocaleString()
                 });
             $scope.newText = '';       
             }
-        }
+        };
        
         $scope.addUser = function(){
-            if($scope.newUser != ''){
-                FirebaseService.addUser({
+         //   console.log("Add user");
+            if($scope.newUser != ''){ // tarkistaa onko kenttä tyhjä
+                if (!FirebaseService.userExists($scope.newUser)){
+                 //   console.log("ei ole");
+                    FirebaseService.addUser({
                     user: $scope.newUser 
-                });
+                    });
+                }
             $scope.activeUser=$scope.newUser;
             $scope.newUser = '';
             }
-        }        
-        */
+        };
+        
 });
--- wk5-36_Chat/src/app/services/firebase_service.js
+++ wk5-36_Chat/src/app/services/firebase_service.js
@@ -1,10 +1,12 @@
-ChatApp.service('FirebaseService', function($firebaseArray) {
+ChatApp.service('FirebaseService', function ($firebase) { // $firebaseArray
     // Keskustele Firebasen kanssa tämän palvelun avulla
-    var usersFirebaseRef = new Firebase("https://vivid-torch-5618.firebaseio.com/users"); 
-    var users = $firebaseArray(usersFirebaseRef);
- /*   
+    var usersFirebaseRef = new Firebase("https://vivid-torch-5618.firebaseio.com/users");
+    var usersSync = $firebase(usersFirebaseRef);  // $firebaseArray
+    var users = usersSync.$asArray();  // $firebaseArray
+        
     var messagesFirebaseRef = new Firebase('https://vivid-torch-5618.firebaseio.com/messages');
-    var messages = $firebaseArray(messagesFirebaseRef);
+    var messagesSync  = $firebase(messagesFirebaseRef);
+    var messages = messagesSync.$asArray();
 
 
     this.addMessage = function (message) {
@@ -22,5 +24,32 @@ ChatApp.service('FirebaseService', function($firebaseArray) {
     this.getUsers = function () {
         return users;
     };
-    */
+    
+    this.userExists = function (user) {
+      //  console.log("user exists?"+user);
+        var isSame = false;
+        
+        usersFirebaseRef.once("value", function(allUsersSnapshot) {
+        allUsersSnapshot.forEach(function(userSnapshot) {
+        // Will be called with a messageSnapshot for each child under the /messages/ node
+        var key = userSnapshot.key();  // e.g. "-JqpIO567aKezufthrn8"
+        var uid = userSnapshot.child("user").val();  // e.g. "barney"
+        if (user===uid){
+       //     console.log("SERV samat");
+            //break
+            isSame = true;
+            return true;  // forEach
+        }
+      //  console.log(uid);
+      });
+    });
+    
+    
+    if (isSame === true) return true;
+    else {
+     //   console.log("ei löytynyt");
+        return false;
+    }
+    };
+
 });
